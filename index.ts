6a0961b63d3973e07d3c0bc4e43e947b08e94b14
Sergei Vorfolomeev <vorfo1897@gmail.com>
2024-01-09 20:43:12 +0300

    solve tasks

--- index.ts
+++ index.ts
@@ -30,3 +30,31 @@ const average = (a: number, b: number): number => {
 };
 
 console.log(average(10,20)) // 15
+
+// Реализуйте функцию square с сигнатурой square(x: number): number вычисляющую квадрат своего аргумента.
+const square = (x: number): number => {
+    return x ** 2
+};
+
+console.log(square(10)) // 100
+
+// Реализуйте функцию cube с сигнатурой cube(x: number): number вычисляющую куб своего аргумента.
+const cube = (x: number): number => {
+    return x ** 3
+};
+
+console.log(cube(10)) // 1000
+
+// Вызовите функции square и cube в цикле от 0 до 9, вычисляя,
+// соответственно квадрат и куб от переменной цикла. Передайте
+// квадрат и куб на каждой итерации в функцию average. Результаты
+// сложите в массив и возвратите из функции calculate.
+const calculate = (): number[] => {
+    let res = []
+    for (let i=0; i<10; i++) {
+        res.push(average(square(i), cube(i)))
+    }
+    return res
+};
+
+console.log(calculate()) // [0, 1, 6, 18, 40, 75, 126, 196, 288,...]
