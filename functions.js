e05527634775c0a3ed2c1c20ac2130aed7982086
Sergei Vorfolomeev <vorfo1897@gmail.com>
2024-01-10 23:36:03 +0300

    solve task

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-// Реализуйте функцию random(min, max), возвращающую псевдо-случайное значение от min до max.
+// 1) Реализуйте функцию random(min, max), возвращающую псевдо-случайное значение от min до max.
 // Используйте Math.random() и Math.floor(). При вызове random(max) нужно считать, что min = 0.
 
 const random = (min = 0, max) => {
@@ -9,7 +9,7 @@ const random = (min = 0, max) => {
     }
 
     const getRandom = () => {
-        let number = Math.random() * max+1
+        let number = Math.random() * max + 1
         return Math.floor(number)
     }
 
@@ -23,12 +23,26 @@ const random = (min = 0, max) => {
 }
 
 const easierSolution = (min, max) => {
-    if(max === undefined) {
+    if (max === undefined) {
         max = min
         min = 0
     }
-    return Math.floor(Math.random() * (max-min+1))
+    return Math.floor(Math.random() * (max - min + 1))
 }
 
-console.log(random(5,10))
-console.log(easierSolution(20,70))
+// console.log(random(5, 10))
+// console.log(easierSolution(20, 70))
+
+// 2) Реализуйте функцию generateKey(length, characters), возвращающую строку случайных символов
+// из набора characters длиной length.
+
+const generateKey = (length, characters) => {
+    let res = ''
+    for (let i = 0; i < length; i++) {
+        const randomNumber = Math.floor(Math.random() * characters.length)
+        res += characters[randomNumber]
+    }
+    return res
+}
+
+console.log(generateKey(16, 'abcdefghijklmnopqrstuvwxyz0123456789')) // 9z0iyu72nuzwlj4z
