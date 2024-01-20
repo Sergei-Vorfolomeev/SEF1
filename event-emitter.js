4d904bacf8692d12b6dca4da56a77c01c9aa75b2
Sergei Vorfolomeev <vorfo1897@gmail.com>
2024-01-20 21:32:18 +0300

    event emitter with closure

--- event-emitter.js
+++ event-emitter.js
@@ -1,29 +1,59 @@
-const EventEmitter = function() {
-    this.events = {}
+// const EventEmitter = function() {
+//     this.events = {}
+// }
+//
+// EventEmitter.prototype.on = function(name, fn) {
+//     if (this.events[name]) {
+//         this.events[name].push(fn)
+//     } else {
+//         this.events[name] = [fn]
+//     }
+// }
+//
+// EventEmitter.prototype.emit = function(name, ...data) {
+//     if (this.events[name]) {
+//         this.events[name].forEach(fn => fn(...data))
+//     } else console.log('no found events')
+//
+// }
+//
+// const ee = new EventEmitter()
+//
+// ee.on('click', (data) => console.log(`CLICK EVENT WITH ${data}`))
+// ee.on('click', (data) => console.log(`SECOND CLICK EVENT WITH ${data}`))
+// ee.on('drop', (data) => console.log(`DROP EVENT WITH ${data}`))
+//
+// ee.emit('click', 'data1')
+// ee.emit('click', 5)
+// ee.emit('drop', true)
+// ee.emit('no-found', 10)
+
+// =====================================================================================
+// Event Emitter with closure
+
+const eventEmitter = () => {
+    const events = {}
+        return {
+            on: (name, fn) => {
+                if (events[name]) events[name].push(fn)
+                else events[name] = [fn]
+            },
+            emit: (name, ...data) => {
+                if (events[name]) {
+                    events[name].forEach(fn => fn(...data))
+                } else console.log('event was not found')
+            },
+        }
+
 }
 
-EventEmitter.prototype.on = function(name, fn) {
-    if (this.events[name]) {
-        this.events[name].push(fn)
-    } else {
-        this.events[name] = [fn]
-    }
-}
+const emitter = eventEmitter()
 
-EventEmitter.prototype.emit = function(name, ...data) {
-    if (this.events[name]) {
-        this.events[name].forEach(fn => fn(...data))
-    } else console.log('no found events')
+emitter.on('click', (data) => console.log(`CLICK EVENT WITH ${data}`))
+emitter.on('click', (data) => console.log(`SECOND CLICK EVENT WITH ${data}`))
+emitter.on('drop', (data) => console.log(`DROP EVENT WITH ${data}`))
 
-}
-
-const ee = new EventEmitter()
-
-ee.on('click', (data) => console.log(`CLICK EVENT WITH ${data}`))
-ee.on('click', (data) => console.log(`SECOND CLICK EVENT WITH ${data}`))
-ee.on('drop', (data) => console.log(`DROP EVENT WITH ${data}`))
-
-ee.emit('click', 'data1')
-ee.emit('click', 5)
-ee.emit('drop', true)
-ee.emit('no-found', 10)
+emitter.emit('click', 'data1')
+emitter.emit('click', 5)
+emitter.emit('drop', true)
+emitter.emit('no-found', 10)
