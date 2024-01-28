96f84e121845ac01389db41dae0014a5a00e3f7b
Sergei Vorfolomeev <vorfo1897@gmail.com>
2024-01-29 00:37:47 +0300

    memoize

---
+++ memoize.js
@@ -0,0 +1,32 @@
+const generateKeys = (args) => {
+   return args.map(arg => arg.toString() + ':' + typeof arg).join('|')
+}
+
+const memoize = (fn) => {
+    const cache = Object.create(null)
+    return (...args) => {
+        const key = generateKeys(args)
+        const value = cache[key]
+        if (value) return value
+        const res = fn(...args)
+        cache[key] = res
+        return res
+    }
+}
+
+const fib = (n) => n <= 2 ? 1 : fib(n-2) + fib(n-1)
+const mFib = memoize(fib)
+
+const speedTest = (name, fn, args, count) => {
+    const tmp = []
+    const start = new Date().getTime()
+    for (let i=0; i<count; i++) {
+        tmp.push(fn(...args))
+    }
+    const end = new Date().getTime()
+    const time = end - start
+    return console.log(`${name} * ${tmp.length}: ${time}ms`)
+}
+
+speedTest('fib', fib, [25], 10000)
+speedTest('mFib', mFib, [25], 10000)
