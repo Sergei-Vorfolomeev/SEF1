c9b1fdf6165b1463aa944aeae4c2e62e1eb8ccfc
Sergei Vorfolomeev <vorfo1897@gmail.com>
2024-01-31 22:44:20 +0300

    wrappers: once

--- wrappers.js
+++ wrappers.js
@@ -14,7 +14,7 @@ const timeout = (ms, fn) => {
     }
 }
 
-const fn = (arg) => console.log('Function has worked')
+const fn = () => console.log('Function has worked')
 
 // const res = timeout(1000, fn)
 // const res2 = timeout(2000, fn)
@@ -30,21 +30,45 @@ const fn = (arg) => console.log('Function has worked')
 //     res5()
 // }, 3500)
 
+// =============================================================================================
+
 const fnWithCallback = (par, callback) => {
     console.log('Function has worked')
     callback(null, par)
 }
 
-const res6 = timeout(1000, fnWithCallback)
-const res7 = timeout(2000, fnWithCallback)
-const res8 = timeout(3000, fnWithCallback)
-const res9 = timeout(4000, fnWithCallback)
-const res10 = timeout(5000, fnWithCallback)
+// const res6 = timeout(1000, fnWithCallback)
+// const res7 = timeout(2000, fnWithCallback)
+// const res8 = timeout(3000, fnWithCallback)
+// const res9 = timeout(4000, fnWithCallback)
+// const res10 = timeout(5000, fnWithCallback)
+//
+// setTimeout(() => {
+//     res6('First', (p1, p2) => (console.log(p2 + ' callback works')))
+//     res7('Second', (p1, p2) => console.log(p2 + ' callback works'))
+//     res8('Third', (p1, p2) => console.log(p2 + ' callback works'))
+//     res9('Fourth', (p1, p2) => console.log(p2 + ' callback works'))
+//     res10('Fifth', (p1, p2) => console.log(p2 + ' callback works'))
+// }, 3500)
 
-setTimeout(() => {
-    res6('First', (p1, p2) => (console.log(p2 + ' callback works')))
-    res7('Second', (p1, p2) => console.log(p2 + ' callback works'))
-    res8('Third', (p1, p2) => console.log(p2 + ' callback works'))
-    res9('Fourth', (p1, p2) => console.log(p2 + ' callback works'))
-    res10('Fifth', (p1, p2) => console.log(p2 + ' callback works'))
-}, 3500)
+// =============================================================================================
+
+//once
+
+const once = fn => (...args) => {
+    if (!fn) {
+        console.log('this function does not exist')
+        return null
+    }
+    const res = fn(...args)
+    fn = null
+    return res
+}
+
+const res11 = once(fn)
+const res12 = res11()
+const res13 = res11()
+console.log(res12)
+console.log(res13)
+
+// =============================================================================================
