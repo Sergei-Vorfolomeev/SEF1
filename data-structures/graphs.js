8dec86698396dbc1e168b9d24abb50b8c4ad52df
Sergei Vorfolomeev <vorfo1897@gmail.com>
2024-02-06 14:10:53 +0300

    create undirected graph with methods add/delete, link/unlink

--- data-structures/graphs.js
+++ data-structures/graphs.js
@@ -4,13 +4,28 @@ class Graph {
         this.vertices = new Map()
     }
 
-    add(data) {
-        const vertex = new Vertex(this, data)
-        const key = data[this.keyField]
+    add(vertex) {
+        //const vertex = new Vertex(this, data)
+        const key = vertex.data[this.keyField]
+        if (!vertex.graph) {
+            vertex.graph = this
+        }
         if (!this.vertices.has(key)) {
             this.vertices.set(key, vertex)
         }
         return vertex
+    }
+
+    delete(vertex) {
+        const key = vertex.data[this.keyField]
+        if (this.vertices.has(key)) {
+            this.vertices.delete(key)
+            delete vertex.graph
+            for (let link of vertex.links) {
+                vertex.unlink(link)
+                link.unlink(vertex)
+            }
+        }
     }
 }
 
@@ -23,33 +38,63 @@ class Vertex {
 
     link(...args) {
         const vertices = new Set(args)
-      //  const keyField = this.graph.keyField
+       // const keyField = this.graph.keyField
         for (let vertex of vertices) {
-          //  const key = vertex.data[keyField]
-            this.links.add(vertex)
+            if (!this.graph.vertices.has(vertex)) {
+                this.graph.add(vertex)
+            }
+            if (!this.links.has(vertex)) {
+                this.links.add(vertex)
+            }
+            if (!vertex.links.has(this)) {
+                vertex.link(this)
+            }
         }
         return this
     }
+
+    unlink(...args) {
+        const vertices = new Set(args)
+        for (let vertex of vertices) {
+            if (this.links.has(vertex)) {
+                this.links.delete(vertex)
+            }
+        }
+    }
 }
 
-
 const graph = new Graph('name')
-const sergey = graph.add({id: 3, name: 'Sergey',})
-const irina = graph.add({id: 4, name: 'Irina',})
-const artem = graph.add({id: 1, name: 'Artem',})
-const vadim = graph.add({id: 5, name: 'Vadim',})
-const fedir = graph.add({id: 2, name: 'Fedir',})
-const jamhur = graph.add({id: 6, name: 'Jamhur',})
+
+const sergey = new Vertex(graph, {id: 3, name: 'Sergey',})
+const irina = new Vertex(graph, {id: 4, name: 'Irina',})
+const artem = new Vertex(graph, {id: 1, name: 'Artem',})
+const vadim = new Vertex(graph, {id: 5, name: 'Vadim',})
+const fedir = new Vertex(graph, {id: 2, name: 'Fedir',})
+const jamhur = new Vertex(graph, {id: 6, name: 'Jamhur',})
+
+graph.add(sergey)
+graph.add(irina)
+graph.add(artem)
+graph.add(vadim)
+graph.add(fedir)
+graph.add(jamhur)
 
 sergey.link(irina, vadim, fedir)
 irina.link(sergey, vadim, sergey)
-artem.link(jamhur)
+artem.link(jamhur, sergey, vadim, sergey, fedir)
 vadim.link(irina, sergey, fedir)
 fedir.link(vadim, sergey)
 jamhur.link(artem)
 
+graph.delete(artem)
+graph.add(artem)
+artem.link(jamhur)
+
+console.log(artem)
+
 for (let vertex of graph.vertices) {
     console.log(vertex)
+    //console.log(vertex.map(el => el.links))
 }
 
 //console.log(graph)
